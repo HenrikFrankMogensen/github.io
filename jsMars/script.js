34f3efcc20dd0aaad0069ebe27cc005fc92b8492
HenrikFrankMogensen <96954982+HenrikFrankMogensen@users.noreply.github.com>
2023-09-28 15:44:49 +0200

    Add files via upload

--- jsMars/script.js
+++ jsMars/script.js
@@ -1,5 +1,8 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+
+//import * as THREE from 'three';
+//import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
